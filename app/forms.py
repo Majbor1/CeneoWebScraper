8c3e40c3cc2afc04ed5adba3de13be8fe57aea54
Marcin Majborski <marcinmajborski@gmail.com>
2025-06-04 14:41:12 +0200

    MM 04.06.2025

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
 from wtforms import Form, StringField, SubmitField, validators
 
 class ProductForm(Form):
-    def __init__(self):
-        product_id = StringField("Identyfikator produktu", 
-                                 name='product_id', 
-                                 id='product_id',
-                                 validators=[
-                                     validators.DataRequired()
-                                 ])
-        submit = SubmitField("Pobierz opinie")
+    product_id = StringField("Kod produktu", 
+                            name='product_id', 
+                            id='product_id',
+                            validators=[
+                                validators.DataRequired(message="Kod produktu jest wymagany"),
+                                validators.Regexp("^[0-9]*$", message="Kod produktu musi zawierać jedynie cyfry"),
+                                validators.Length(min=5, max=10, message="Kod produktu musi zawierać od 5 do 10 cyfr")
+                            ])
+    submit = SubmitField("Pobierz opinie")
     
--- app/models/product.py
+++ app/models/product.py
@@ -30,6 +30,32 @@ class Product:
             "product_name": self.product_name,
             "stats": self.stats
         }
+    
+    def info_from_dict(self, info):
+        self.product_id = info['product_id']
+        self.product_name = info['product_name']
+        self.stats = info['stats']
+
+    def reviews_from_dict(self, reviews_list):
+        for review_dict in reviews_list:
+            review = Review()
+            review.form_dict(review_dict)
+            self.reviews.append(review)
+
+
+    def if_not_exists(self):
+        next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        response = requests.get(next_page, headers=headers)
+        if response.status_code == 200:
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            opinions_count = extract(page_dom, "a.product-review__link > span")
+            if opinions_count:
+                return False
+            else: 
+                return "Dla produktu o podanym kodzie nie ma jeszcze opinii"
+        else:
+            return "Produkt o podanym kodzie nie istnieje"
+        
 
     def extract_name(self):
         next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
